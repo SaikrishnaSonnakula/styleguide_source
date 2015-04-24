63965212936c98249d03c9f9108cc2c95d13f2e2
Shane Russell <shane1337@gmail.com>
2015-04-23 17:58:59 -0700

    Add icons to styleguide

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,4 +2,13 @@
 
 module.exports = function(grunt) {
   require('build-plugin')(grunt);
+
+  grunt.config('copy.development.files', 
+    grunt.config('copy.development.files').concat([{
+      cwd: 'app',
+      expand: true,
+      src: ['icon/**/*'],
+      dest: 'target/development'
+    }])
+  );
 };
