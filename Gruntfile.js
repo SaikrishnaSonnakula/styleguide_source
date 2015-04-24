812ec5313b4ed587bb9772571d09bb4d3d5f2a16
Shane Russell <shane1337@gmail.com>
2015-04-24 11:18:17 -0700

    Add icon library to dist deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,12 +3,21 @@
 module.exports = function(grunt) {
   require('build-plugin')(grunt);
 
-  grunt.config('copy.development.files', 
+  grunt.config('copy.development.files',
     grunt.config('copy.development.files').concat([{
       cwd: 'app',
       expand: true,
       src: ['icon/**/*'],
       dest: 'target/development'
+    }])
+  );
+
+  grunt.config('copy.dist.files',
+    grunt.config('copy.dist.files').concat([{
+      cwd: 'app',
+      expand: true,
+      src: ['icon/**/*'],
+      dest: 'target/dist'
     }])
   );
 };
