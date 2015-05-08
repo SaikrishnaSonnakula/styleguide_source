615410d29a94508dd4873b1a2ce0f0451919627d
Shane Russell <shane1337@gmail.com>
2015-05-08 16:56:01 -0700

    Responsive documentation

--- app/scripts/create_examples.js
+++ app/scripts/create_examples.js
@@ -17,14 +17,23 @@ define(['jquery'], function($){
       $exampleNote.remove();
 
       var $code = $("<div class='code'></div>");
+
+      var $extraCodeEl = $el.find(".code");
+      var $extraCode = "";
+      $extraCodeEl.removeClass("code");
+      $extraCodeEl.remove();
+      if($extraCodeEl.html()) {
+        $extraCode = $("<pre></pre>").append($("<code></code>").text(trimMarkup($extraCodeEl.html())));
+        $extraCode.attr("class", $extraCodeEl.attr("class"));
+      }
+
+      $code.append($extraCode)
       $code.append($("<pre></pre>").append($("<code class='language-markup'></code>").text(trimMarkup($el.html()))));
-      //$code.prepend("<h4>Code</h4>");
 
       var $result = $el.clone();
       $result.removeClass();
       $result = $("<div class='result-pane'></div>").append($result);
       $result = $("<div class='result'></div>").append($result);
-      //$result.prepend("<h4>Result</h4>");
       $result.append($exampleNote);
 
       $el.html("");
