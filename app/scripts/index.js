442aaf25ade55e5b710b90e62f8ecef514be9b7a
Shane Russell <shane1337@gmail.com>
2015-06-03 22:48:32 -0700

    Important new styles

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -5,11 +5,20 @@ require.config({
 });
 
 define(['jquery', 'add_colors', 'create_examples', 'vendor/fixedsticky'], function($, addColors, createExamples) {
+  var easterEgg = function() {
+    $("#name-field").keyup(function() {
+      if($('#name-field').val().trim() === "vlad") {
+        $(".example, h2, h3, p").addClass("animate-spin");
+        $("nav ul").html("VLAD MODE<br>WELCOME TO STYLETOWN!");
+      }
+    });
+  };
 
   $(function() {
     $(".fixedsticky").fixedsticky();
     addColors();
     createExamples();
+    easterEgg();
 
     $("#modal-link").click(function() {
       $("body, #my-modal").addClass("modal-showing");
