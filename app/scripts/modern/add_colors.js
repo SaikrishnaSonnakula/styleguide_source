5a94a7315f666659024c743d9aeaf0ab61a2b96b
Shane Russell <shane1337@gmail.com>
2015-10-26 16:58:30 -0700

    Add missing files (doh)

---
+++ app/scripts/modern/add_colors.js
@@ -0,0 +1,40 @@
+import $ from "jquery";
+
+var colorSections = [
+  {
+    name: "KP Colors",
+    colors: [
+      'kp-blue',
+      'interactive-blue',
+    ]
+  },
+];
+
+var getHex = function($el) {
+  var rgb = $el.css('backgroundColor').match(/\d+/g);
+  var r   = parseInt(rgb[0], 10);
+  var g   = parseInt(rgb[1], 10);
+  var b   = parseInt(rgb[2], 10);
+  return '#'+ r.toString(16) + g.toString(16) + b.toString(16);
+};
+
+export default function() {
+  colorSections.forEach(function(section){
+    var $section = $('<div class="color-section"></div>');
+
+    $('#colors').append($section);
+    $section.before('<h3>' + section.name + '</h3>');
+
+    section.colors.forEach(function(color){
+      var $color = $('<div class="color"></div>');
+      $section.append($color);
+
+      var $swatch = $('<div class="swatch '+ color +'"></div>');
+      $color.append($swatch);
+
+      $color.append('<code>$' + color + '</code><br/>');
+      $color.append('<code>' + getHex($swatch) + '</code>');
+    })
+
+  })
+};
---
+++ app/scripts/modern/index.js
@@ -0,0 +1,40 @@
+require('fixedsticky.css');
+require('prism.css');
+require('../../scss/modern/index.scss');
+
+import $ from 'jquery';
+import addColors from './add_colors';
+import createExamples from '../create_examples';
+import _prism from 'prism';
+import _fixedSticky from 'fixedsticky';
+
+var easterEgg = function() {
+  $("#name-field").keyup(function() {
+    if($('#name-field').val().trim() === "vlad") {
+      $(".example, h2, h3, p").addClass("animate-spin");
+      $("nav ul").html("VLAD MODE<br>WELCOME TO STYLETOWN!");
+    }
+  });
+};
+
+$(function() {
+  $(".fixedsticky").fixedsticky();
+  addColors();
+  createExamples();
+  easterEgg();
+
+  $("#modal-link").click(function() {
+    $("#my-modal").addClass("modal-showing");
+    $("#my-modal").attr('aria-hidden','false');
+    $(".modal-inner" ,"#my-modal").attr('tabindex', '0').focus();
+  });
+
+  $(".modal-close, .modal-fade-screen").click(function() {
+    $("#my-modal").removeClass("modal-showing");
+    $("#my-modal").attr('aria-hidden','true');
+    $(".modal-inner" ,"#my-modal").removeAttr('tabindex');
+  });
+
+
+  $("main").show();
+});
