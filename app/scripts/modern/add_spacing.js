4f45bd0220b00420ff17753bce88f40258f6595c
Pamela Ocampo <pamela.ocampo@kp.org>
2016-01-19 12:35:53 -0800

    [STYLE-1: Pam] Actual Font Sizes show in pixels

--- app/scripts/modern/add_spacing.js
+++ app/scripts/modern/add_spacing.js
@@ -1,13 +1,21 @@
 import $ from "jquery";
 
+export function addSpacing() {
+  $(".spacing-example").each(function(i, el){
+    var $example = $(el);
+    var value = $example.css("top"); //hack to get info to Javascript to display on html
+    var $value = $("<div class='spacing-value'>" + value + "</div>");
 
+    $example.append($value);
+  });
+}
 
-export default function() {
-	$(".spacing-example").each(function(i, el){
-		var $example = $(el);
-		var value = $example.css("top"); //hack to get info to Javascript to display on html
-		var $value = $("<div class='spacing-value'>" + value + "</div>");
-
-		$example.append($value);
-	});
+export function addFontSizes() {
+  $('#typography-section .font-variable').each(function(i, el){
+    var $section = $(el);
+    var $example = $section.children('[class$="font-size"], [class$="font-family"]');
+    var value = $example.css('font-size');
+    var $value = $("<div class='auxiliary-text'>Actual size: " + value + "</div>");
+    $section.append($value);
+  });
 }
