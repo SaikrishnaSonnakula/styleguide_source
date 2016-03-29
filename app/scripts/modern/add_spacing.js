86dd7ab7414eb6127a007eb3af17adff1daaa717
Lina Qu <lqu@thoughtworks.com>
2016-03-29 12:55:32 -0700

    [Fix: Lina] fix eslint warnings, there are too many of them.

--- app/scripts/modern/add_spacing.js
+++ app/scripts/modern/add_spacing.js
@@ -1,10 +1,10 @@
-import $ from "jquery";
+import $ from 'jquery';
 
 export function addSpacing() {
-  $(".spacing-example").each(function(i, el){
+  $('.spacing-example').each(function(i, el){
     var $example = $(el);
-    var value = $example.css("top"); //hack to get info to Javascript to display on html
-    var $value = $("<div class='spacing-value'>" + value + "</div>");
+    var value = $example.css('top'); //hack to get info to Javascript to display on html
+    var $value = $('<div class="spacing-value">' + value + '</div>');
 
     $example.append($value);
   });
@@ -15,13 +15,13 @@ export function addFontSizes() {
     var $section = $(el);
     var $example = $section.children('[class$="font-size"], [class$="font-family"]');
     var value = $example.css('font-size');
-    var $value = $("<div class='auxiliary-text'>Actual size: " + value + " or " + toEm(value) + "em.</div>");
+    var $value = $('<div class="auxiliary-text">Actual size: ' + value + ' or ' + toEm(value) + 'em.</div>');
     $section.append($value);
   });
 }
 
 function toEm(px){
   let value = parseFloat(px);
-  let emSize = parseFloat($("body").css("font-size"));
+  let emSize = parseFloat($('body').css('font-size'));
   return Math.round((value / emSize) * 100)/100;
 }
--- app/scripts/modern/responsive_table.js
+++ app/scripts/modern/responsive_table.js
@@ -1,5 +1,5 @@
-import $ from "jquery";
-import _ from "underscore";
+import $ from 'jquery';
+import _ from 'underscore';
 
 var breakPoints = {
   '$mobile': {
